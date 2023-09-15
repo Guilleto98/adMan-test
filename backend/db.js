e71bcfddb347ca836629230d3465b8e7b48edffb
guilleto98 <guillermo98td@gmail.com>
2023-09-15 13:18:32 -0300

    Frontend:
    - Create component listItem
    
    Backend:
    
    - Create envs
    - Fix bug on database

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,20 @@
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
+
+console.log({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+  })
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root1234',
-  database: 'db_spotify',
-  port: 3306,
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
 });
 
 module.exports = connection;
--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,21 @@ const cors = require("cors");
 const { getToken, spotifySearch } = require("./services/services");
 const bodyParser = require("body-parser");
 
-const mysql = require('mysql2')
-const connection = require('./db');
+const mysql = require("mysql2");
+const connection = require("./db");
 
-const sql = 'INSERT INTO searchHistory (inputText) VALUES (?)';
+const insertQuery = "INSERT INTO searchHistory (inputText) VALUES (?)";
 
 const app = express();
 const PORT = 3000;
 
 connection.connect((error) => {
   if (error) {
-    console.error('Error de conexión a la base de datos:', error);
+    console.error("Error de conexión a la base de datos:", error);
   } else {
-    console.log('Conexión exitosa a la base de datos MySQL');
+    console.log("Conexión exitosa a la base de datos MySQL");
   }
 });
-
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -32,25 +31,21 @@ app.get("/api/artist", async (req, res) => {
 
 app.post("/api/search", async (req, res) => {
   const response = await spotifySearch(req.body.search);
-  console.log(response)
-  res.send(response);
+  console.log(response);
+
   try {
-  connection.query(sql, [req.body.search], (err, result) => {
-    if (err) {
-      console.error("Error al insertar en la base de datos:", err);
-    } else {
-      console.log("Datos insertados en la base de datos");
-    }
-    
-    // Cierra la conexión después de completar la consulta
-    connection.end();
-  });
-} catch (error) {
-  console.error("Error inesperado:", error);
-}
-
+    connection.query(insertQuery, [req.body.search], (err, result) => {
+      if (err) {
+        console.error("Error al insertar en la base de datos:", err);
+      } else {
+        console.log("Datos insertados en la base de datos");
+      }
+      res.send(response);
+    });
+  } catch (error) {
+    console.error("Error inesperado:", error);
+  }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
